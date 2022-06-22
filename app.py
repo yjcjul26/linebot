088c5340d609e8563e1e3d21cb5e15300bd5ccfb
yjcjul26 <yjcjul26@gmail.com>
2022-06-22 20:49:38 +0800

    v3

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    print("LineBot debug=Request body: " + body)
     # handle webhook body
     try:
         handler.handle(body, signature)
