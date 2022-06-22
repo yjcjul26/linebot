0f4be44f47282ba1a4f0f86a0d8aa0e7cc721b2f
yjcjul26 <yjcjul26@gmail.com>
2022-06-22 20:47:26 +0800

    v2

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+   
+    message = TextSendMessage(text="just do it,ok? "+event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
